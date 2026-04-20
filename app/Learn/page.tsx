b455e7cf19cc97cb4ecc9f79c08bb76543073eda
unknown <davidrevaldo0606@gmail.com>
2026-04-19 22:17:28 -0700

    feat: new learpage

---
+++ app/Learn/page.tsx
@@ -0,0 +1,14 @@
+export default function LearnPage () {
+    return ( 
+        <div>
+            <div>
+                <img src="/user.svg" alt="Design" width={50} height={50} />
+                <p>Design</p>
+                </div>
+                <div>
+                    <p>Figma</p>
+                    <p>8 hours ago</p>
+            </div>
+        </div>
+    );
+}
