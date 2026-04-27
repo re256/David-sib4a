9f0831641f012ed8b10be3a274157e81f952abfb
unknown <you@example.com>
2026-04-27 12:29:25 +0800

    third

--- app/Learn/page.tsx
+++ app/Learn/page.tsx
@@ -1,14 +1,28 @@
 export default function LearnPage () {
     return ( 
-        <div>
-            <div>
-                <img src="/user.svg" alt="Design" width={50} height={50} />
-                <p>Design</p>
+        <section className="h-screen bg-gray-100 flex gap-4">
+        <div className="bg-blue-300 p-4 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950 ">
+            <div className="flex gap-3">
+                <img src="/user.svg" alt="user"/>
+                <p className="font-medium">Design</p>
                 </div>
                 <div>
-                    <p>Figma</p>
-                    <p>8 hours ago</p>
+                    <h2 className="text-4xl font-black">Figma</h2>
+                    <p className="font-bold">8 hours ago</p>
             </div>
         </div>
+
+            <div className="bg-blue-300 p-4 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className="flex gap-3">
+                <img src="/user.svg" alt="user"/>
+                <p className="font-medium">Design</p>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-black">Figma</h2>
+                    <p className="font-bold">8 hours ago</p>
+            </div>
+        </div>
+        
+        </section>
     );
 }
