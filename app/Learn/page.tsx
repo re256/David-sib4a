513dad72d19426fc52a2aa8ef4193b916e80a616
unknown <davidrevaldo0606@gmail.com>
2026-05-03 13:43:37 +0800

    tugas

--- app/Learn/page.tsx
+++ app/Learn/page.tsx
@@ -1,13 +1,13 @@
 export default function LearnPage () {
     return ( 
         <section className="h-screen bg-gray-100 flex gap-4">
-        <div className="bg-blue-300 p-4 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950 ">
-            <div className="flex gap-3">
-                <img src="/user.svg" alt="user"/>
-                <p className="font-medium">Design</p>
+        <div className="bg-blue-300 p-4 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className="flex gap-3 items-center">
+                <img src="/palette.svg" alt="palette"/>
+                <p className="font-semibold">Design</p>
                 </div>
                 <div>
-                    <h2 className="text-4xl font-black">Figma</h2>
+                    <h2 className="text-3xl font-bold">Figma</h2>
                     <p className="font-bold">8 hours ago</p>
             </div>
         </div>
@@ -18,7 +18,7 @@ export default function LearnPage () {
                 <p className="font-medium">Design</p>
                 </div>
                 <div>
-                    <h2 className="text-4xl font-black">Figma</h2>
+                    <h2 className="text-3xl font-bold">Figma</h2>
                     <p className="font-bold">8 hours ago</p>
             </div>
         </div>
---
+++ app/task-2/page.tsx
@@ -0,0 +1,68 @@
+export default function TaskPage() {
+  return (
+  <section className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-4 p-8 font-sans">
+    
+    <div className="w-full max-w-4xl flex flex-col gap-6"> 
+
+      <div className="w-full h-64 flex flex-row gap-6">
+        <div className="bg-cyan-100 flex-[50%] p-6 relative rounded-3xl flex flex-col justify-between text-cyan-900">
+          <div className="flex gap-3">
+            <img src="/palette.svg" alt="palette" className="w-4 h-5 object-contain"/>
+            <p className="text-sm font-medium flex items-center gap-3">Design</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold ">Adobe Photoshop</h2>
+            <p className="text-sm font-semibold mt-1">in 3 days</p>
+          </div>
+        </div>
+
+        <div className="bg-green-100 flex-[50%] p-6 relative rounded-3xl flex flex-col justify-between text-green-900">
+          <div className="flex gap-3">
+            <img src="/brand-tabler.svg" alt="brand-tabler" className=" w-4 h-5 object-contain" />
+            <p className="text-sm font-medium flex items-center gap-3">AI</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold">DALL·E 2, Midjourney, Stable Diffusion</h2>
+            <p className="text-sm font-semibold mt-1">in 5 days</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full h-70 flex flex-row gap-6">
+        <div className="bg-purple-100 flex-1 p-6 h-64 rounded-4xl flex flex-col justify-between text-purple-900">
+          <div className="flex gap-3">
+            <img src="/palette.svg" alt="palette" className=" w-4 h-5 object-contain" />
+            <p className="text-sm font-medium flex items-center gap-3">Design</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold">Figma</h2>
+            <p className="text-sm font-semibold mt-1">8 hours ago</p>
+          </div>
+        </div>
+
+        <div className="bg-orange-100 flex-1 p-6 h-64 rounded-4xl flex flex-col justify-between text-orange-900">
+          <div className="flex gap-3">
+            <img src="/code.svg" alt="code" className="w-4 h-5 object-contain" />
+            <p className="text-sm font-medium flex items-center gap-3">Codding</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold">Python</h2>
+            <p className="text-sm font-semibold mt-1">2 days ago</p>
+          </div>
+        </div>
+
+        <div className="bg-pink-100 flex-1 p-6 h-64 rounded-4xl flex flex-col justify-between text-pink-900">
+          <div className="flex gap-3">
+            <img src="/palette.svg" alt="palette" className="w-4 h-5 object-contain" />
+            <p className="text-sm font-medium flex items-center gap-3">Design</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold">Sketch</h2>
+            <p className="text-sm font-semibold mt-1">4 days ago</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  );
+}
